627af54e3b1032401f57993d450ac68df39cf7af
williamlab123 <williamprv@outlook.com>
2024-03-30 14:29:51 -0300

    feat - added flying eneny

--- Assets/scripts/FlyinEnemy.cs
+++ Assets/scripts/FlyinEnemy.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 public class FlyingEnemyController : MonoBehaviour
 {
     public float moveSpeed = 1f;
-    public float minY = -1f;
-    public float maxY = 0.5f;
 
+    float projectSpeed = 10f;
+    public float minY = -0.5f;
+    public float maxY = 1f;
+    public GameObject objectToInstantiate; // The prefab you want to instantiate
     public Transform player;
 
-    void Start()
-    {
-        // player = GameObject.FindGameObjectWithTag("Player").transform;
-    }
+    public float attackSpeed = 1f; // Attacks per second
+    private float nextAttackTime = 0.0f;
 
     void Update()
     {
@@ -24,5 +24,27 @@ public class FlyingEnemyController : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
         float clampedY = Mathf.Clamp(transform.position.y, minY, maxY);
         transform.position = new Vector3(transform.position.x, clampedY, transform.position.z);
+
+        // Check if it's time for the next attack
+        if (Time.time >= nextAttackTime)
+        {
+            // Perform the attack
+            Attack();
+
+            // Calculate the time for the next attack
+            nextAttackTime = Time.time + 3;
+        }
+    }
+
+    void Attack()
+    {
+        // Calculate direction towards the player at the time of instantiation
+        Vector3 directionToPlayer = (player.position - transform.position).normalized;
+
+        // Instantiate the object at the current position
+        GameObject instantiatedObject = Instantiate(objectToInstantiate, transform.position, Quaternion.identity);
+
+        // Move the instantiated object towards the player's direction
+        instantiatedObject.GetComponent<Rigidbody2D>().velocity = directionToPlayer * projectSpeed;
     }
 }
--- Assets/scripts/ProjectScript.cs
+++ Assets/scripts/ProjectScript.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class ProjectScript : MonoBehaviour
 {
+
+    public HeroKnight player;
+    public float speed = 10f;
+
+    
+
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // player = GetComponent<HeroKnight>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3.MoveTowards(transform.position, new Vector3(-6, -1, 0), speed * Time.deltaTime);
+
     }
 }
