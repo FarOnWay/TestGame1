2e4bb402bd09e05e0b93ea1c1bda9fe1084c1ab5
williamlab123 <williamprv@outlook.com>
2024-05-12 13:47:35 -0300

    .

--- Assets/scripts/PetFollowPlayer.cs
+++ Assets/scripts/PetFollowPlayer.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PetFollowPlayer : MonoBehaviour
@@ -10,7 +8,7 @@ public class PetFollowPlayer : MonoBehaviour
     public float heightOffset; // Add this field
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         Move();
     }
