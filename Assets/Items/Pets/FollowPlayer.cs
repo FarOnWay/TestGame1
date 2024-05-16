6f22c0275b5feabf3e5029eb72966f68e7566580
williamlab123 <williamprv@outlook.com>
2024-05-16 17:25:00 -0300

    feat - added an lightorb like terraria + added a script to pets follow player

---
+++ Assets/Items/Pets/FollowPlayer.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform playerPos;
+    public float speed;
+    public float stoppingDistance;
+    public float heightOffset;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    public virtual void Move()
+    {
+        Debug.Log("Moving towards player");
+        Vector3 targetPos = playerPos.position + new Vector3(0, heightOffset, 0);
+        float distance = Vector3.Distance(transform.position, targetPos);
+        if (distance > stoppingDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        }
+
+        if (distance > 15)
+        {
+            transform.position = targetPos;
+        }
+    }
+}
