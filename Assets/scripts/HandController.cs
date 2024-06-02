c0dd6d80955d10eb2631aed9ab9f5aa3b1b34191
williamlab123 <williamprv@outlook.com>
2024-06-02 00:01:18 -0300

    feat - now no more using animation to move the hand, its hard coded + the attack speed of the equipped item now is working

--- Assets/scripts/HandController.cs
+++ Assets/scripts/HandController.cs
@@ -1,39 +1,114 @@
 using UnityEngine;
+using System.Collections;
 
 public class HandController : MonoBehaviour
 {
     public SpriteRenderer sprite;
+    public Transform player; // Reference to the player's transform
     public Item equippedItem;
-
-    Transform positionWhenLookingLeft;  //looking right: x - 52.03 y - 1.72 
-                                        //looking left: x - 0.139 y - 0.354
+    public float rotationSpeed = 0; // Speed of the rotation
 
     void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
-        positionWhenLookingLeft = GetComponent<Transform>();
     }
 
     void setSprite()
     {
-        if (equippedItem == null) sprite.sprite = null;
+        if (equippedItem == null)
+            sprite.sprite = null;
+        else
+        {
+            sprite.sprite = equippedItem.icon;
 
-        else sprite.sprite = equippedItem.icon;
+
+        }
     }
+
+
+    void getInfoFromEquippedItem()
+    {
+        if (equippedItem != null)
+        {
+            // Get the item type of the equipped item
+            ItemType itemType = equippedItem.itemType;
+
+            // Handle different item types
+            switch (itemType)
+            {
+                case ItemType.Consumable:
+                    // Do something for consumable items
+                    Debug.Log("Equipped item is a consumable.");
+                    break;
+                case ItemType.Attack:
+                    // Cast the equipped item to AttackItem and get the attack speed
+                    Attack.AttackItem attackItem = equippedItem as Attack.AttackItem;
+                    if (attackItem != null)
+                    {
+                        float attackSpeed = attackItem.attackSpeed;
+                        rotationSpeed = attackSpeed * 360f;
+                        Debug.Log("Attack speed: " + attackSpeed);
+                    }
+                    break;
+                case ItemType.Defense:
+                    // Do something for defense items
+                    Debug.Log("Equipped item is a defense item.");
+                    break;
+                case ItemType.Material:
+                    // Do something for material items
+                    Debug.Log("Equipped item is a material.");
+                    break;
+            }
+        }
+    }
+    private bool isAttacking = false;
 
     void Update()
     {
-        positionWhenLookingLeft.position = new Vector3(10f, 10f, 0);
         setSprite();
+        getInfoFromEquippedItem();
+
         float inputX = Input.GetAxis("Horizontal");
+
         if (inputX > 0)
         {
-            GetComponent<SpriteRenderer>().flipX = false;
+            sprite.flipX = false;
         }
         else if (inputX < 0)
         {
-             GetComponent<SpriteRenderer>().flipX = true;
-
+            sprite.flipX = true;
         }
+
+        // If the attack button is initially pressed
+        if (Input.GetButtonDown("Fire1") && !isAttacking)
+        {
+            StartCoroutine(Attack());
+        }
+    }
+    IEnumerator Attack()
+    {
+        isAttacking = true;
+
+        // Determine the rotation direction based on the player's facing direction
+        Vector3 rotationDirection = sprite.flipX ? Vector3.forward : Vector3.back;
+
+        // Set the sword's local rotation to 4.4 degrees in the z-axis
+        transform.localRotation = Quaternion.Euler(0, 0, 4.4f);
+
+        // Perform a full rotation
+        for (float t = 0; t < 1; t += Time.deltaTime)
+        {
+            transform.RotateAround(player.position, rotationDirection, rotationSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        // Continue rotating while the attack button is held down
+        while (Input.GetButton("Fire1"))
+        {
+            transform.RotateAround(player.position, rotationDirection, rotationSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        isAttacking = false;
     }
 }
