47ee1363bf6c83ac97cbdd1776d529071d1de6e4
williamlab123 <williamprv@outlook.com>
2024-06-14 20:39:35 -0300

    feat - added a targetDummy to test weapons and show the damage taken

--- Assets/scripts/ShowDamageTaken.cs
+++ Assets/scripts/ShowDamageTaken.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShowDamageTaken : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text damageText;
+    public Transform TargetPos;
+    public Vector3 offset;
+
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 screenPosition = Camera.main.WorldToScreenPoint(TargetPos.position + offset);
+        damageText.rectTransform.position = screenPosition;
+    }
+
+    public void ShowDamageTakenOnScreen(string damage, bool isCritical)
+    {
+        if (isCritical)
+        {
+            damage = "<color=red>" + damage + "</color>";
+        }
+        damage = "<color=yellow>" + damage + "</color>";
+
+        damageText.text = damage;
+        //   damageText.color = color;
+
+        StartCoroutine(Cleardamage(2f));
+    }
+
+    private IEnumerator Cleardamage(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        damageText.text = "";
     }
 }
